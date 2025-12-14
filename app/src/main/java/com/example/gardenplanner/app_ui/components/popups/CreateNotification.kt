dbccfeeb0452efcf0d1827f4b78ecb338ffe76e0
TheLuckyman30 <bnauta0@gmail.com>
2025-12-14 05:30:14 -0500

    Added a date picker to the form

--- app/src/main/java/com/example/gardenplanner/app_ui/components/popups/CreateNotification.kt
+++ app/src/main/java/com/example/gardenplanner/app_ui/components/popups/CreateNotification.kt
@@ -11,10 +11,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,6 +38,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.time.ZonedDateTime
 
 @Composable
 fun CreateNotification(close: () -> Unit) {
@@ -43,7 +55,8 @@ fun CreateNotification(close: () -> Unit) {
 @Composable
 fun CreateNotificationContent(close: () -> Unit) {
     var name by remember { mutableStateOf("") }
-    var date by remember { mutableStateOf("") }
+    var date by remember { mutableStateOf<ZonedDateTime>(ZonedDateTime.now()) }
+    var showDatePicker by remember { mutableStateOf(false) }
 
     Column {
         Box(modifier = Modifier
@@ -88,33 +101,19 @@ fun CreateNotificationContent(close: () -> Unit) {
             }
         )
         Spacer(modifier = Modifier.height(20.dp))
-        BasicTextField(
-            value = date,
-            onValueChange = { newDate ->
-                date = newDate
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(36.8696.dp)
-                .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 8.56821.dp))
-                .padding(start = 8.875.dp, top = 8.875.dp, end = 8.875.dp, bottom = 8.875.dp),
-            textStyle = TextStyle(fontSize = 16.sp),
-            decorationBox = { innerTextField ->
-                Box(
-                    modifier = Modifier.fillMaxHeight(),
-                    contentAlignment = Alignment.CenterStart
-                ) {
-                    if (date.isEmpty()) {
-                        Text(
-                            "Date",
-                            fontWeight = FontWeight(500),
-                            color = Color(0xFF2F7564)
-                        )
-                    }
-                    innerTextField()
-                }
+        Row (modifier = Modifier
+            .height(36.8696.dp)
+            .background(color = Color.White, shape = RoundedCornerShape(size = 8.56821.dp))
+            .padding(start = 8.875.dp, top = 8.875.dp, end = 8.875.dp, bottom = 8.875.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Text("Date",
+                fontWeight = FontWeight(500),
+                color = Color(0xFF2F7564))
+            Spacer(modifier = Modifier.width(10.dp))
+            IconButton(onClick = { showDatePicker = true }, modifier = Modifier.size(20.dp)) {
+                Icon(contentDescription = "Date Picker", imageVector = Icons.Default.CalendarMonth)
             }
-        )
+        }
         Spacer(modifier = Modifier.height(20.dp))
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
             Row {
@@ -151,8 +150,48 @@ fun CreateNotificationContent(close: () -> Unit) {
                         )
                     )
                 }
-
             }
         }
+        if (showDatePicker) {
+            DatePickerModalInput(
+                onDateSelected = { selectedMillis ->
+                    selectedMillis?.let {
+                        val instant = java.time.Instant.ofEpochMilli(selectedMillis)
+                        val zonedDateTime = instant.atZone(java.time.ZoneId.systemDefault())
+                        date = zonedDateTime
+                    }
+                    showDatePicker = false
+                },
+                onDismiss = { showDatePicker = false }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModalInput(
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
     }
 }
