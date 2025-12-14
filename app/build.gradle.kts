70e96f27a7fe3681b3c4c21a95505dce0182c57f
Selin Bacaz <144861936+selinAbacaz@users.noreply.github.com>
2025-12-13 23:09:53 -0500

    big ui changes to landing page and individual plant page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,10 @@ android {
         version = release(36)
     }
 
+    dependencies {
+        implementation("androidx.compose.material:material-icons-extended")
+    }
+
     defaultConfig {
         applicationId = "com.example.gardenplanner"
         minSdk = 24
@@ -50,6 +54,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
