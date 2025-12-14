5eba7665f041856a28af940bc485e9b0e9090b30
TheLuckyman30 <bnauta0@gmail.com>
2025-12-14 03:31:08 -0500

    Added topbar to notifcations page

--- app/src/main/java/com/example/gardenplanner/app_ui/screens/Notifications.kt
+++ app/src/main/java/com/example/gardenplanner/app_ui/screens/Notifications.kt
@@ -1,5 +1,6 @@
 package com.example.gardenplanner.app_ui.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,54 +19,38 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.gardenplanner.app_ui.components.NotificationCard
+import com.example.gardenplanner.app_ui.components.NotificationNav
 import com.example.gardenplanner.utils.classes.Notification
 import com.example.gardenplanner.utils.classes.Plant
-import java.time.ZonedDateTime
 
-fun generateNotification(plant: Plant): Notification {
-    var newInterval: Long = 6
-    if (plant.growingRequirements.waterRequirement.lowercase().contains("high")) {
-        newInterval = 1
-    } else if (plant.growingRequirements.waterRequirement.lowercase().contains("moderate")) {
-        newInterval = 3
-    }
-    val newTime = ZonedDateTime.now()
-    val addedTime = newTime.plusDays(newInterval)
-    return Notification(plant, addedTime, newInterval)
-}
-
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Notifications(userPlants: List<Plant>,
                   userNotifications: List<Notification>,
                   addNotification: (Notification) -> Unit) {
-    Box (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 100.dp)
-            .background(Color(0xFF9CC7B9), RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
-            .verticalScroll(rememberScrollState()),
+    Scaffold(
+        topBar = { NotificationNav(
+            userPlants,
+            userNotifications,
+            addNotification
+        ) }
     ) {
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp),
-            contentAlignment = Alignment.Center) {
-            Column(Modifier.fillMaxWidth(0.9F)) {
-                userNotifications.forEach { notification ->
-                    NotificationCard(notification)
-                    Spacer(modifier = Modifier.height(10.dp))
-                }
-                TextButton(onClick = {
-                    userPlants.forEach { plant ->
-                        val existingNotification = userNotifications.firstOrNull { notification ->
-                            notification.plant == plant
-                        }
-                        if (existingNotification == null) {
-                            val newNotification = generateNotification(plant)
-                            addNotification(newNotification)
-                        }
+        Box (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 100.dp)
+                .background(Color(0xFF9CC7B9), RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
+                .verticalScroll(rememberScrollState()),
+        ) {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+                contentAlignment = Alignment.Center) {
+                Column(Modifier.fillMaxWidth(0.9F)) {
+                    userNotifications.forEach { notification ->
+                        NotificationCard(notification)
+                        Spacer(modifier = Modifier.height(10.dp))
                     }
-                }) {
-                    Text("Test")
                 }
             }
         }
